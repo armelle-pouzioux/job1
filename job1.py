23e357b14edbc0c21f77a6578e613e5955949eba
starm3113 <armelle.pzx@gmail.com>
2024-11-04 10:28:18 +0100

    job1

---
+++ job1.py
@@ -0,0 +1,6 @@
+valeur1= int(input("definir valeur1: "))
+valeur2= int(input("definir valeur2: "))
+if valeur1==valeur2 :
+ print("valeur1 est egal a valeur2")
+else :
+ print("les deux valeurs ne sont pas egals") 
